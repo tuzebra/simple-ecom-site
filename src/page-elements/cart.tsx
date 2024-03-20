d15686ee13409b6061b8255d2d2a6781a160d5e5
Huy Phan <huyphan29@gmail.com>
2024-03-20 17:34:08 +0100

    update page cart

--- src/page-elements/cart.tsx
+++ src/page-elements/cart.tsx
@@ -1,12 +1,35 @@
+import { useEffect } from 'react';
 import PageTitle from '@/components/page-title';
+import LoadingBar from '@/components/loading-bar';
+import { useFetchCart } from '@/apis/cart';
 
 //////////////////////// "REACT COMPONENT" FUNCTIONS ////////////////////////
 
 const Cart = () => {
+  const {loading, response, execute} = useFetchCart();
+
+  // load cart when component was mounted
+  useEffect(() => {
+    execute({});
+  }, [execute]);
+
   return (
-    <div className='container'>
-      <PageTitle title='Cart' />
-    </div>
+    <>
+      {loading && <LoadingBar>Loading your cart...</LoadingBar>}
+      <div className='container'>
+        <PageTitle title='Cart' />
+        <div>
+          {response && response.data && response.data.products.map((product, index) => (
+            <div key={`${product.id}-${index}`}>
+              <img src={product.thumbnail} alt={product.title} />
+              <h3>{product.title}</h3>
+              <p>{product.price}</p>
+              <p>{product.quantity}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
